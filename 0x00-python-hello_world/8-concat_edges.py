943639883d95e359765b8e8f0ca3a1b7d90ac51c
KK2313 <j.i.kimingichi@gmail.com>
2023-06-06 03:27:51 -0700

    Corrections

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an object-centered, interpreted programming\
-        language that combines incredible capability with very straightforward syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+        language that combines incredible capability with very straightforward syntax"
+print(str[39:67] + str[107:112] + str[:6])
